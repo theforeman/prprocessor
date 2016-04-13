7964cd7cc08d51a2d3fdabee13344b5b8c273c26
Lukas Zapletal <lzap+git@redhat.com>
2016-04-13 11:21:53 +0200

    Added close_inactive weekly script

---
+++ scripts/close_inactive.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+require 'octokit'
+require 'pp'
+require 'date'
+require 'yaml'
+
+CONFIG = {}
+CONFIG.merge!(YAML.load_file('config/close_inactive.yaml'))
+CONFIG.merge!(YAML.load_file('config/local_close_inactive.yaml')) if File.exists?('config/local_close_inactive.yaml')
+
+THRESHOLD = DateTime.now << CONFIG[:keep]
+COMMENT = <<EOC
+Thank you for your contribution, @%s! This PR has been inactive for #{CONFIG[:keep]} months, closing for now.
+Feel free to reopen when you return to it. This is an automated process.
+EOC
+
+c = Octokit::Client.new(:access_token => ENV['GITHUB_OAUTH_TOKEN'])
+CONFIG[:repos].each do |repo|
+  query = "repo:#{repo} type:pr state:open label:\"Waiting on contributor\" updated:\"<#{THRESHOLD}\""
+  result = c.search_issues(query, :per_page => CONFIG[:max_closed], :sort => 'updated_at', :order => 'asc')
+  puts "Pull requests older than #{THRESHOLD}: #{result[:total_count]}"
+  result[:items].each do |pr|
+    title = pr[:title]
+    number = pr[:number]
+    user = pr[:user][:login]
+    updated_at = pr[:updated_at].to_datetime
+    labels = c.labels_for_issue(repo, number).collect{ |x| x[:name] }
+    if updated_at < THRESHOLD && labels.include?("Waiting on contributor")
+      puts "Closing #{number} (#{title}) with latest update #{updated_at}"
+      c.add_comment(repo, number, COMMENT % user) if CONFIG[:add_comment]
+      c.add_labels_to_an_issue(repo, number, CONFIG[:labels]) if CONFIG[:add_labels]
+      c.close_pull_request(repo, number) if CONFIG[:close]
+    end
+  end
+end
+
