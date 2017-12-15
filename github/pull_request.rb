theforeman/prprocessor

commit fe16b84b3a033fd860f3859d4aedba90b9e3bf1a

Use dependency injection for the github client

github/pull_request.rb
@@ -7,12 +7,11 @@ class PullRequest
   attr_accessor :raw_data, :title, :issue_numbers, :repo,
     :number, :client, :commits
 
-  def initialize(repo, raw_data)
+  def initialize(repo, raw_data, client)
     self.repo     = repo
     self.raw_data = raw_data
     self.title    = raw_data['title']
     self.number   = raw_data['number']
-    self.client   = Octokit::Client.new(:access_token => ENV['GITHUB_OAUTH_TOKEN'])
 
     # Sometimes the GitHub API returns a 404 immediately after PR creation
     Retriable.retriable :on => Octokit::NotFound, :interval => 2, :tries => 10 do

app.rb
@@ -29,7 +29,8 @@ post '/pull_request' do
   raise "repo not configured" if Repository[repo_name].nil?
   repo = Repository[repo_name]
 
-  pull_request = PullRequest.new(repo, payload['pull_request'])
+  client = Octokit::Client.new(:access_token => ENV['GITHUB_OAUTH_TOKEN'])
+  pull_request = PullRequest.new(repo, payload['pull_request'], client)
   pr_number = pull_request.raw_data['number']
 
   halt if event == 'pull_request' && ['closed', 'labeled', 'unlabeled'].include?(action)

scripts/close_inactive.rb
@@ -45,7 +45,7 @@ def close_prs(repo, config, label, time, message)
       c.add_labels_to_an_issue(repo, number, CONFIG[:labels]) if CONFIG[:add_labels]
       c.close_pull_request(repo, number) if CONFIG[:close]
 
-      pr_obj = PullRequest.new(config, pr)
+      pr_obj = PullRequest.new(config, pr, c)
       pr_obj.issue_numbers.map { |issue| Issue.new(issue) }.each do |issue|
         issue.set_assigned(nil)
         issue.set_status(Issue::NEW)

commit 3d73b730b80b3504526ea335c5604cddae1ecb33

Set the client propery
fe16b84b3a033fd860f3859d4aedba90b9e3bf1a caused a regression where the
@client propery was no longer set.

github/pull_request.rb
@@ -12,6 +12,7 @@ class PullRequest
     self.raw_data = raw_data
     self.title    = raw_data['title']
     self.number   = raw_data['number']
+    self.client   = client
 
     # Sometimes the GitHub API returns a 404 immediately after PR creation
     Retriable.retriable :on => Octokit::NotFound, :interval => 2, :tries => 10 do