027ce754a75fe53ee854208b4f6c5a37279ce2c0
Eric D. Helms <ericdhelms@gmail.com>
2017-12-22 20:59:05 -0500

    Ensure entire config is set when updating webhook

--- scripts/update_webhook.rb
+++ scripts/update_webhook.rb
@@ -7,6 +7,10 @@ if ENV['GITHUB_AUTH_TOKEN'].nil?
   abort("GITHUB_AUTH_TOKEN is needed to access the GitHub API")
 end
 
+if ENV['GITHUB_SECRET_TOKEN'].nil?
+  abort("GITHUB_SECRET_TOKEN is needed")
+end
+
 repos = YAML.load_file('../config/repos.yaml').keys
 
 Octokit.configure do |c|
@@ -14,7 +18,7 @@ Octokit.configure do |c|
 end
 
 new_url = "https://prprocessor.theforeman.org/pull_request"
-old_url = "http://prprocessor.theforeman.org/pull_request"
+old_url = "https://prprocessor.theforeman.org/pull_request"
 
 repos.each do |repo|
   puts "Checking #{repo} : https://api.github.com/repos/#{repo}/hooks"
@@ -29,7 +33,11 @@ repos.each do |repo|
       repo,
       hook_to_update.id,
       'web',
-      {:url => new_url}
+      {
+        :url => new_url,
+        :content_type => "json",
+        :secret => "'#{ENV['GITHUB_SECRET_TOKEN']}'"
+      }
     )
 
     puts "Hook updated for #{repo}"
