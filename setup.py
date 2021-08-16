80976354219d854846513a82ff24924dedc168ff
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-08-16 18:42:37 +0200

    Move from Pipfile to setup.py
    Pipenv ends up being not great.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='prprocessor',
+    version='0.1',
+    description='Handles GitHub PRs and syncing to Redmine and Jenkins',
+    url='https://github.com/theforeman/prprocessor/tree/app',
+    author='Ewoud Kohl van Wijngaarden',
+    author_email='ewoud+python@kohlvanwijngaarden.nl',
+    license='MIT',
+    packages=['prprocessor'],
+    install_requires=[
+        'PyYAML',
+        'octomachinery',
+        'python-redmine',
+    ],
+    package_data={'prprocessor': ['config/*.yaml']},
+    python_requires='>= 3.7',
+)
