theforeman/prprocessor

commit 3f95206f1be89eb45d4dcc45b6203080923a6c34

Factor out branch -> version prefix logic

prprocessor/__main__.py
@@ -165,6 +165,23 @@ async def get_issues_from_pr(pull_request: Mapping) -> tuple[IssueValidation, Co
 
     return verify_issues(config, issue_ids), invalid_commits
 
+def get_version_prefix_from_branch(target_branch: str) -> str | None:
+    if target_branch.endswith('-stable'):
+        # Handle a branch like 3.0-stable. This means we get an additional prefix of 3.0. which
+        # allows get_latest_open_version to find the right version
+        version_prefix = f'{target_branch.removesuffix("-stable")}.'
+    elif target_branch.startswith('KATELLO-'):
+        # Handle a branch like KATELLO-4.9. This means we get an additional prefix of 4.9. which
+        # allows get_latest_open_version to find the right version
+        version_prefix = f'{target_branch.removeprefix("KATELLO-")}.'
+    elif target_branch in ('main', 'master', 'develop', 'deb/develop', 'rpm/develop'):
+        # Development branches don't have a version prefix so they really use the latest
+        version_prefix = ''
+    else:
+        return None
+
+    return version_prefix
+
 
 async def run_pull_request_check(pull_request: Mapping, check_run=None) -> None:
     github_api = RUNTIME_CONTEXT.app_installation_client
@@ -339,18 +356,8 @@ async def on_pr_merge(*, pull_request: Mapping, **_kw) -> None:
 
     repository = pull_request['base']['repo']['full_name']
     target_branch = pull_request['base']['ref']
-    if target_branch.endswith('-stable'):
-        # Handle a branch like 3.0-stable. This means we get an additional prefix of 3.0. which
-        # allows get_latest_open_version to find the right version
-        version_prefix = f'{target_branch.removesuffix("-stable")}.'
-    elif target_branch.startswith('KATELLO-'):
-        # Handle a branch like KATELLO-4.9. This means we get an additional prefix of 4.9. which
-        # allows get_latest_open_version to find the right version
-        version_prefix = f'{target_branch.removeprefix("KATELLO-")}.'
-    elif target_branch in ('main', 'master', 'develop', 'deb/develop', 'rpm/develop'):
-        # Development branches don't have a version prefix so they really use the latest
-        version_prefix = ''
-    else:
+    version_prefix = get_version_prefix_from_branch(target_branch)
+    if version_prefix is None:
         logger.info('Unable to set fixed in version for %s branch %s in PR %s',
                     repository, target_branch, pull_request['number'])
         return

commit bf04d6fbc49b764c4c5ac6ec91a791ec076e6850

Use Python 3.9 compatible syntax
Fixes: 3f95206f1be89eb45d4dcc45b6203080923a6c34 ("Factor out branch -> version prefix logic")

prprocessor/__main__.py
@@ -165,7 +165,7 @@ async def get_issues_from_pr(pull_request: Mapping) -> tuple[IssueValidation, Co
 
     return verify_issues(config, issue_ids), invalid_commits
 
-def get_version_prefix_from_branch(target_branch: str) -> str | None:
+def get_version_prefix_from_branch(target_branch: str) -> Optional[str]:
     if target_branch.endswith('-stable'):
         # Handle a branch like 3.0-stable. This means we get an additional prefix of 3.0. which
         # allows get_latest_open_version to find the right version